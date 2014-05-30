11fe0923eca03b7783d7ccedce7954d45843a300
nicolas-naresh <nicolas.naresh@ithands.net>
2014-05-30 12:36:51 +0530

    added way/generators and bootstraper to composer.json file

--- app/config/app.php
+++ app/config/app.php
@@ -119,6 +119,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+                'Way\Generators\GeneratorsServiceProvider',
+                'Cornford\Bootstrapper\BootstrapServiceProvider'
 
 	),
 
@@ -185,6 +187,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+                'Bootstrap'       => 'Cornford\Bootstrapper\Facades\Bootstrap',
 
 	),
 
--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'database',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'Rubi@123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
