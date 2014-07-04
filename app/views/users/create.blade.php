9dccc40fa48b568c28126912ddd601e05f1adf81
Jack Braj <jack.braj@ithands.net>
2014-07-04 09:42:40 +0530

    ref #0001393 cancel button routing issue fixed

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -16,10 +16,10 @@
       {{ Form::open(array('url' => URL::route("userStore"), 'method' => 'post', 'role' => 'form', 'class' => 'form-horizontal')) }}
         <legend class="form-signin-heading">Add New User</legend>
         @include("users.form", array('user'=>$user))
-        
+
         <div class="form-group">
           <div class="col-sm-6 col-sm-offset-2">
-            <a class="btn btn-danger pull-left" href="{{ URL::previous() }}">Cancel</a>
+            <a class="btn btn-danger pull-left" href="{{ URL::route('usersListing') }}">Cancel</a>
             <input class="btn btn-primary pull-right" type="submit" value="Save User">
           </div>
         </div>
