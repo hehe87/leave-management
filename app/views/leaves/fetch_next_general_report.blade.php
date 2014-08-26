beafb5c86ae1b4ce5ec865196f6b9203aebeb778
nicolas-naresh <nicolas.naresh@ithands.net>
2014-08-26 17:26:09 +0530

    CSS updated for leave report

--- app/views/leaves/fetch_next_general_report.blade.php
+++ app/views/leaves/fetch_next_general_report.blade.php
@@ -1,9 +1,17 @@
 <?php $i = 1;?>
 @while($i<=$lastDay)
+	@if($i == 1)
+		<tr>
+			<td colspan="{{ (1 + count($allUsers)) }}" style="text-align: left;" class="g-report-month-header">
+				<?php $dt = new DateTime(date("Y") . "-" . sprintf("%02s", $month) . "-1" ); ?>
+				{{ $dt->format("M") }}
+			</td>
+		</tr>
+	@endif
 	<tr>
 		<th>
 			<?php $dt = new DateTime(date("Y") . "-" . sprintf("%02s", $month) . "-" . $i); ?>
-			{{ $dt->format("d-M") }}
+			{{ $dt->format("d") }}
 		</th>
 		@foreach($allUsers as $user)
 			<td>
